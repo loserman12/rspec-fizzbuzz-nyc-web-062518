6fc4c04ae91376886a263ec145d32d9285fcc201
Your Name <you@example.com>
2018-06-12 10:38:13 -0400

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,18 @@
 def fizzbuzz(num)
-  if (num % 5 == 0) && (num % 3 == 0)
+  if (num % 3 == 0) && (num % 5 == 0)
+# the % operator divides the num by the other number which we set to 3 and 5 .
+#if it gives us a remainder of 0 it means it is divisible by that number
     return "FizzBuzz"
 
-  elsif num % 3 == 0
+
+  elsif (num % 3 == 0)
     return "Fizz"
 
-  elsif num % 5 == 0
+  elsif (num % 5 == 0)
     return "Buzz"
 
   else
     return
+
   end
 end
