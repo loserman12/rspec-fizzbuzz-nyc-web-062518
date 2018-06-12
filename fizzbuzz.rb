204e4c7e914be6eb740e9b18d68631261d799e74
Your Name <you@example.com>
2018-06-12 10:18:03 -0400

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,14 @@
-def fizzbuzz(int)
-  if int % 3==0 && int % 5==0
-    "FizzBuzz"
-  elsif int % 3==0
-    "Fizz"
-  elsif int % 5==0
-    "Buzz"
+def fizzbuzz(num)
+  if (num % 5 == 0) && (num % 3 == 0)
+    return "FizzBuzz"
+
+  elsif num % 3 == 0
+    return "Fizz"
+
+  elsif num % 5 == 0
+    return "Buzz"
+
   else
-    nil
+    return
   end
 end
